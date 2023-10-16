1f5d4d86924bf882dd83cc33091536906a9cde41
TroyAraneta <145010838+TroyAraneta@users.noreply.github.com>
2023-10-16 20:53:59 +0800

    Araneta_ChineseZodiac.py

--- Araneta_ChineseZodiac.py
+++ Araneta_ChineseZodiac.py
@@ -1,5 +1,5 @@
-ZSigns = ["Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit",
-          "Dragon", "Snake", "Horse", "Goat", "Monkey"]
+CZSigns = ["Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit",
+           "Dragon", "Snake", "Horse", "Goat", "Monkey"]
 
 InputCZS = int(input("Enter a year and know its Chinese Zodiac Sign: "))
 
@@ -9,4 +9,4 @@ elif InputCZS > 9999:
     print("Year can only be 4 digits or less.")
 else:
     i = (InputCZS - 1) % 12
-    print("Your Year's Chinese Zodiac Sign is", ZSigns[i])
+    print("Your Year's Chinese Zodiac Sign is", CZSigns[i])
